eec6ee445eac84f5ec7d8cadee06719ce5c7b2aa
JJK <96130975+johaannx@users.noreply.github.com>
2023-09-27 11:34:04 +0530

    comit2

--- TEST1/app.py
+++ TEST1/app.py
@@ -9,29 +9,36 @@ users = [
     {"username": "johann", "password": "johann123"},
 ]
 
+# Initialize error message as None
+error_message = None
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Pass the error message to the template if it exists
+    return render_template("index.html", error=error_message)
 
 
 @app.route("/login", methods=["POST"])
 def login():
+    global error_message  # Use a global variable to store the error message
     username = request.form.get("username")
     password = request.form.get("password")
 
     for user in users:
         if user["username"] == username and user["password"] == password:
-            # Successful login, redirect to the dashboard or home page
+            # Successful login, reset error_message and redirect to the dashboard or home page
+            error_message = None
             return redirect(url_for("dashboard"))
 
-    # If login fails, return to the login page with an error message
-    return render_template("index.html", error="Invalid username or password")
+    # If login fails, set the error_message and return to the login page
+    error_message = "Invalid username or password"
+    return redirect(url_for("index"))
 
 
 @app.route("/dashboard")
 def dashboard():
-    return "Welcome to your dashboard!"
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
